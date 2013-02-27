4843cffb8dfe73703e14a88b037a99875d2b98f5
twojung <skysky00700@gmail.com>
2013-02-27 11:55:31 +0900

    add index.php, php.php

---
+++ php_test.php
@@ -0,0 +1,13 @@
+<html>
+        <body>
+                Current time is 
+14:57:47(by php) <br />
+        <script type="text/javascript">
+                var d = new Date();
+                var curr_hour = d.getHours();
+                var curr_min = d.getMinutes();
+                var curr_sec = d.getSeconds();
+                document.write('Current time is '+curr_hour+":"+curr_min+":"+curr_sec+" (by javascript)");
+        </script>
+        <body>
+</html>
